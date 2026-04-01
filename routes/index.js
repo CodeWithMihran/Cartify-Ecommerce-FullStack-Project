921bbe0ccac13b9cd6cce18a2da4cc38bc5f6119
CodeWithMihran <sohail.mihran@gmail.com>
2026-04-01 20:13:40 +0530

    Updated the UI/UX of the project and added delete product feature in cart

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,26 @@ router.get("/logout", function(req, res) {
     res.redirect("/");
 });
 
+router.get("/removefromcart/:productid", isLoggedIn, async function (req, res) {
+    try {
+        let user = await userModel.findOne({ email: req.user.email });
+        
+        // Find the index of the specific item to remove only ONE instance 
+        // (in case they added the same bag twice)
+        const index = user.cart.indexOf(req.params.productid);
+
+        if (index > -1) {
+            user.cart.splice(index, 1); // Removes 1 element at that index
+            await user.save();
+            req.flash("success", "Removed from cart");
+        } else {
+            req.flash("error", "Item not found in cart");
+        }
+
+        res.redirect("/cart");
+    } catch (err) {
+        res.send(err.message);
+    }
+});
+
 module.exports = router;
